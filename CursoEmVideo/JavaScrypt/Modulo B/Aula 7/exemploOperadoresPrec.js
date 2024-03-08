19991a4b6496819b07b57929dded6f33cf8e86af
Jean Lucas Bertran <jeanbertran@outlook.com>
2024-03-08 18:39:50 -0300

    aula 7
    anotações da aula 7

---
+++ CursoEmVideo/JavaScrypt/Modulo B/Aula 7/exemploOperadoresPrec.js
@@ -0,0 +1,14 @@
+
+ let a = 5 + 3
+ console.log(a)
+ let b = a % 5
+ console.log(b)
+ let c = 5 * b ** 2
+ console.log(c)
+ let d = 10 - a / 2
+ console.log(d)
+ let e = 6 * 2 / d
+ console.log(e)
+ let f = b % e + 4 / e
+ console.log(f)
+ 
