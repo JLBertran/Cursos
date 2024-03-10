ecea0d1f27ae59599df0f287fc3c5fc3509a45a1
Jean Lucas Bertran <jeanbertran@outlook.com>
2024-03-10 08:52:01 -0300

    aula 8
    anotações operadores relacionais, lógicos e ternários

---
+++ CursoEmVideo/JavaScrypt/Modulo B/Aula 8/ExemploOperadoresRel.js
@@ -0,0 +1,12 @@
+  let a = 5 > 2
+  console.log(a)
+  let b = 7 < 4
+  console.log(b)
+  let c = 8 >= 8
+  console.log(c)
+  let d = 9 <= 7
+  console.log(d)
+  let e = 5 == 5 
+  console.log(e)
+  let f = 4 != 4
+  console.log(f)
